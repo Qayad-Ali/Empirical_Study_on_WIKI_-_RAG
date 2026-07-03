c0433f988e3925a1b178e13549a4a932dda159c2
Qayad-Ali <aliqayad2@gmail.com>
2026-07-03 22:21:00 +0530

    mistaken

--- test_setup.py
+++
@@ -1,6 +0,0 @@
-import ollama
-import config
-response=ollama.chat(model=config.GEN_MODEL,messages=[{"role":"user","content":"say hello in 5 words"}],)
-print("reply:",response["message"]["content"])
-print("input token:",response["prompt_eval_count"])
-print("output token:",response["eval_count"])
